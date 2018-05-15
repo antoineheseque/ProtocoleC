eabe9bdd8b53de302b49d137476b8f32f790dba5
LouisJ <louis.jeannin@isen.yncrea.fr>
2018-05-15 15:22:49 +0200

    lire php 80%

--- ProtocoleC/file.c
+++ ProtocoleC/file.c
@@ -27,39 +27,21 @@ Scene readFile() {
 	//Scene scene;
 	Camera cam;
 	Object * objects = malloc(sizeof(Object));
-	FILE *fp;
-	errno_t err;
+	FILE * fichier = fopen("main.scene", "r");
 
-	err = fopen_s(&fp, "main.scene", "r");
-	if (err != 0)
+	if (fichier != NULL)
 	{
 		printf("Erreur lors de l'ouverture du fichier.\n");
 		return;
 	}
-
-	const size_t line_size = 100;
-	char* line = malloc(line_size);
-	int lineCounter = 0;
-	while (fgets(line, line_size, fp) != NULL) {
-		lineCounter++;
-		if (line == 1) {
-			fscanf(err, "%f;%f;%f", cam.position.x, cam.position.y, cam.position.z);
-		}
-		else if(line == 2) {
-			fscanf(err, "%f;%f;%f", cam.direction.x, cam.direction.y, cam.direction.z);
-		}
-		else if(line == 3) {
-			fscanf(err, "%d", cam.screenWidth);
-		}
-		else if(line == 4) {
-			fscanf(err, "%d", cam.screenHeight);
-		}
-		else if(line == 5) {
-			fscanf(err, "%s;%f;%f;%f;%f;%f;%f", objects.type, objects.position.x, objects.position.y, objects.position.z, objects.size.x, objects.size.y, objects.size.z);
-		}
-		printf("%d: %s", lineCounter, line);
+	else
+	{
+		fscanf(fichier, "%f;%f;%f", &cam.position.x, &cam.position.y, &cam.position.z);
+		fscanf(fichier, "%f;%f;%f", &cam.direction.x, &cam.direction.y, &cam.direction.z);
+		fscanf(fichier, "%d", &cam.screenWidth);
+		fscanf(fichier, "%d", &cam.screenHeight);
+		fscanf(fichier, "%s;%f;%f;%f;%f;%f;%f", &objects.type, &objects.position.x, &objects.position.y, &objects.position.z, &objects.size.x, &objects.size.y, &objects.size.z);
 	}
-	free(line);    // dont forget to free heap memory
 
 	//scene.camera = cam;
 	//debugScene(scene); // Check in console if scene is successfully created from file data.
