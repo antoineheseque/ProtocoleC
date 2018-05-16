b5e0b72ab4ae2e60a9ecb2b1a83d1754c8f23552
Antoine Hésèque <hesequeantoine@gmail.com>
2018-05-16 13:47:24 +0200

    Correction mathC.c

--- ProtocoleC/objects.c
+++ ProtocoleC/objects.c
@@ -14,29 +14,24 @@ char * getObjectType(ObjectType type) {
 	}
 }
 
-Vector3 plan(Vector3 vect, Vector3 pixel, Vector3 a, Vector3 b, Vector3 c){
-	
+Vector3 plan(Vector3 vect, Vector3 pixel, Vector3 v1, Vector3 v2, Vector3 v3){
 	Vector3 inter;
-	int t = (-a * pixel.x - b * pixel.y - c * pixel.z) / (a.x * a + a.y * b + a.y * c);
 	int x,y,z;
-	int a = (b.y - a.y)*(c.z - a.z)-(b.z - a.z)*(c.y - a.y);
-	int b = (b.z - a.z)*(c.x - a.x)-(b.x - a.x)*(c.z - a.z);
-	int c = (b.x - a.x)*(c.y - a.y)-(b.y - a.y)*(c.x - a.x);
-	int t = (-a * pixel.x - b * pixel.y - c * pixel.z) / (a.x * a + a.y * b + a.y * c);
+	int a = (v2.y - v1.y)*(v3.z - v1.z)-(v2.z - v1.z)*(v3.y - v1.y);
+	int b = (v2.z - v1.z)*(v3.x - v1.x)-(v2.x - v1.x)*(v3.z - v1.z);
+	int c = (v2.x - v1.x)*(v3.y - v1.y)-(v2.y - v1.y)*(v3.x - v1.x);
+	int t = (-a * pixel.x - b * pixel.y - c * pixel.z) / (vect.x * a + vect.y * b + vect.y * c);
 	
 	if (t >= 0){ //il y a intersection entre le rayon et le plan
 		
 		inter.x = pixel.y + vect.y * t;
 		inter.y = pixel.x + vect.x * t;
 		inter.z = pixel.z + vect.z * t;
-		
-		return(inter);
 	}
 	else{
 		inter.x = 0;
 		inter.y = 0;
 		inter.z = 0;
-
-		return(inter);
 	}
+	return(inter);
 }
--- ProtocoleC/ray.c
+++ ProtocoleC/ray.c
@@ -3,7 +3,10 @@
 
 Ray getRayFromPixel(Camera cam, int x, int y) {
 	Ray ray;
-	
+	Vector3 dir;
+	dir.x = 5;
+	ray.direction = dir;
+
 	// Code pour tracer le rayon
 
 	return ray;
