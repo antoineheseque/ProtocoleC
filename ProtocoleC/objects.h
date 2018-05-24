51f66c3e8c42db5f8134b8066b285a863eb8a275
Louis JEANNIN <38085824+ElNordon@users.noreply.github.com>
2018-05-24 11:49:20 +0200

    Update objects.h

--- ProtocoleC/objects.h
+++ ProtocoleC/objects.h
@@ -19,6 +19,7 @@ typedef struct object {
 	char type[4];
 } Object;
 
+Vector3 checkPolygone(Ray ray, int nbPts, Vector3 * pts);
 Vector3 CollideWithPlan(Ray ray, Vector3 normale, Vector3 center, Vector3 size);
 Vector3 CollideWithSphere(Ray ray, Vector3 orig, int radius);
 #endif
